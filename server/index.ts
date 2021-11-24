2e1b8aeda902595c74040af4e62e4f4c9a0cb344
hernan-cosin <78047711+hernan-cosin@users.noreply.github.com>
2021-11-24 18:05:08 -0300

    added dist folder to express app

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,12 @@ app.get("/env-vars", (req, res) => {
   });
 });
 
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/dist/index.html");
+});
+
 app.listen(port, () => {
   console.log(`App running in port ${port}`);
 });
